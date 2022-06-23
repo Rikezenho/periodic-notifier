0f3d31267060f149f16475dd920e2361a0996855
Henrique <rikezenho@gmail.com>
2022-06-22 22:54:10 -0300

    fix: remove pwa files but add manifest generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,23 @@
 import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
+import webmanifest from "astro-webmanifest";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  experimental: {
+    integrations: true,
+  },
+  integrations: [
+    react(),
+    webmanifest({
+      name: "Periodic Notifier", // required
+      short_name: "Periodic Notifier",
+      description: "Um simples app para notificar de tempos em tempos",
+      start_url: "/",
+      theme_color: "#333",
+      background_color: "#FFF",
+      display: "standalone",
+    }),
+  ],
 });
